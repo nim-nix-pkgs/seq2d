124ce47819f9d674be997d464c59ef4496aba8be
hugosenari <hugosenari@gmail.com>
2022-10-12 10:34:07 -0300

    chore: re index seq2d

--- v0_1_1/flake.nix
+++ v0_1_1/flake.nix
@@ -7,11 +7,11 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-seq2d-master.flake = false;
-  inputs.src-seq2d-master.ref   = "refs/heads/master";
-  inputs.src-seq2d-master.owner = "avahe-kellenberger";
-  inputs.src-seq2d-master.repo  = "seq2d";
-  inputs.src-seq2d-master.type  = "github";
+  inputs.src-seq2d-v0_1_1.flake = false;
+  inputs.src-seq2d-v0_1_1.ref   = "refs/tags/v0.1.1";
+  inputs.src-seq2d-v0_1_1.owner = "avahe-kellenberger";
+  inputs.src-seq2d-v0_1_1.repo  = "seq2d";
+  inputs.src-seq2d-v0_1_1.type  = "github";
   
   inputs."nimtest".owner = "nim-nix-pkgs";
   inputs."nimtest".ref   = "master";
@@ -24,13 +24,13 @@
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-seq2d-master"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-seq2d-v0_1_1"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-seq2d-master";
+    src  = deps."src-seq2d-v0_1_1";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
